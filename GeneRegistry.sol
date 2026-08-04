b8c556b877641b5bbfb8cd297b9675c9d3a8c136
Khatisani <khatisanimongwe@gmail.com>
2026-08-04 14:50:18 +0200

    feat: added the structure of every GeneRecord.

--- GeneRegistry.sol
+++ GeneRegistry.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.20;
 
 // Gene registry Contract 
-// Define the data structure of the gene record
+contract GeneRegistry{
 
+// Define the data structure of the gene record
+    struct GeneRecord {
+        string speciesName;
+        string traitType;
+        address researcher;
+        string sequence;
+    }
 
 // Events 
 
 
 // Function to register a gene 
+    function registerGene(string memory _speciesName, string memory _traitType, string memory _sequence) external {
+        Gene
+        // If not, create a new gord and emit an eventene rec
+    }
 
 
 // Function to retrieve a gene based on its sequence
@@ -16,3 +27,5 @@ pragma solidity ^0.8.20;
 
 // Function to check if a sequence string is already registered
 
+
+}
