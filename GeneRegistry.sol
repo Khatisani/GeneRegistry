f5b933c782548ef3171ef8e992c9e638d0e7ed85
Khatisani <khatisanimongwe@gmail.com>
2026-08-04 14:40:10 +0200

    doc: Determine the initial structure of the contract.

---
+++ GeneRegistry.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Gene registry Contract 
+// Define the data structure of the gene record
+
+
+// Events 
+
+
+// Function to register a gene 
+
+
+// Function to retrieve a gene based on its sequence
+
+
+// Function to check if a sequence string is already registered
+
