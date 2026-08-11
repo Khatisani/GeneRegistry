aa6a88ddd7e536b9c8e1da0106b3f963de0f6f5e
Khatisani <khatisanimongwe@gmail.com>
2026-08-11 12:44:13 +0200

    feat: made registerGene function a payable function for the registration fee. Added validation to check if the fee amount is not correct.

--- GeneRegistry.sol
+++ GeneRegistry.sol
@@ -29,8 +29,11 @@ contract GeneRegistry{
 mapping(bytes32 => GeneRecord) private records;
 
 // Function to register a gene 
+// Fee registration of 0.001 eth
+    uint256 public fee = 0.001 ether;
+
     function registerGene(
-        string memory _speciesName, string memory _traitType, string memory _sequence) external {
+        string memory _speciesName, string memory _traitType, string memory _sequence) external payable {
         
         if (bytes(_speciesName).length == 0) revert emptyField("speciesName");
         if (bytes(_traitType).length == 0) revert emptyField("traitType");
@@ -38,6 +41,10 @@ mapping(bytes32 => GeneRecord) private records;
 
         if (records[keccak256(bytes(_sequence))].exists) {
             revert duplicateSequence(_sequence);
+        }
+
+        if (msg.value < fee){
+            revert ("Insufficient funds. Registration fee is 0.001 ETH");
         }
         
 
